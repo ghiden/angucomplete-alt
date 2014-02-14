b104c42421101ee620ae376cc55ef2dea43d4b4c
Hidenari Nozaki <hidenari@movio.co>
2014-02-14 15:51:14 +1300

    Just laying out test environment

---
+++ test/angucompleteSpec.js
@@ -0,0 +1,10 @@
+'use strict';
+
+describe('angucomplete', function() {
+  beforeEach(module('angucomplete'));
+
+  it('should pass this simple test', function() {
+    expect(true).toBe(true);
+  });
+
+});
