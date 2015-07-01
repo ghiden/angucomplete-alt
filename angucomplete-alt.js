55535bcaf173465753424a5a92f5df54fb912867
Hidenari Nozaki <hidenari@gmail.com>
2015-07-02 06:10:05 +1200

    Fix #191

--- angucomplete-alt.js
+++ angucomplete-alt.js
@@ -210,7 +210,9 @@
           if (key) {
             keys= key.split('.');
             result = obj;
-            keys.forEach(function(k) { result = result[k]; });
+            for (var i = 0; i < keys.length; i++) {
+              result = result[keys[i]];
+            }
           }
           else {
             result = obj;
