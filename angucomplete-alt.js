f520f61ae73f1a86643261de2919690841a08ddf
Ryan Vall <rvall@patriotsoftware.com>
2017-03-27 14:18:28 -0400

    remove comment

--- angucomplete-alt.js
+++ angucomplete-alt.js
@@ -299,10 +299,7 @@
       function handleOverrideSuggestions(event) {
         if (scope.overrideSuggestions &&
             !(scope.selectedObject && scope.selectedObject.originalObject === scope.searchStr)) {
-          // if (event) {
-          //   event.preventDefault();
-          // }
-
+              
           // cancel search timer
           $timeout.cancel(searchTimer);
           // cancel http request
